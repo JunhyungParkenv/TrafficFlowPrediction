9091aae11ced716923cd9d2a002293f8802ff86e
JunhyungParkenv <145532861+JunhyungParkenv@users.noreply.github.com>
2024-04-26 20:05:12 -0500

    ..

--- model/model.py
+++ model/model.py
@@ -1,93 +1,49 @@
-"""
-Defination of NN model
-"""
-from keras.layers import Dense, Dropout, Activation
-from keras.layers.recurrent import LSTM, GRU
-from keras.models import Sequential
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Dropout, LSTM, GRU
+from tensorflow.keras.models import Sequential
 
 
 def get_lstm(units):
-    """LSTM(Long Short-Term Memory)
-    Build LSTM Model.
-
-    # Arguments
-        units: List(int), number of input, output and hidden units.
-    # Returns
-        model: Model, nn model.
-    """
-
-    model = Sequential()
-    model.add(LSTM(units[1], input_shape=(units[0], 1), return_sequences=True))
-    model.add(LSTM(units[2]))
-    model.add(Dropout(0.2))
-    model.add(Dense(units[3], activation='sigmoid'))
-
+    model = Sequential([
+        LSTM(units[1], input_shape=(units[0], 1), return_sequences=True),
+        LSTM(units[2]),
+        Dropout(0.2),
+        Dense(units[3], activation='sigmoid')
+    ])
     return model
 
 
 def get_gru(units):
-    """GRU(Gated Recurrent Unit)
-    Build GRU Model.
-
-    # Arguments
-        units: List(int), number of input, output and hidden units.
-    # Returns
-        model: Model, nn model.
-    """
-
-    model = Sequential()
-    model.add(GRU(units[1], input_shape=(units[0], 1), return_sequences=True))
-    model.add(GRU(units[2]))
-    model.add(Dropout(0.2))
-    model.add(Dense(units[3], activation='sigmoid'))
-
+    model = Sequential([
+        GRU(units[1], input_shape=(units[0], 1), return_sequences=True),
+        GRU(units[2]),
+        Dropout(0.2),
+        Dense(units[3], activation='sigmoid')
+    ])
     return model
 
 
 def _get_sae(inputs, hidden, output):
-    """SAE(Auto-Encoders)
-    Build SAE Model.
-
-    # Arguments
-        inputs: Integer, number of input units.
-        hidden: Integer, number of hidden units.
-        output: Integer, number of output units.
-    # Returns
-        model: Model, nn model.
-    """
-
-    model = Sequential()
-    model.add(Dense(hidden, input_dim=inputs, name='hidden'))
-    model.add(Activation('sigmoid'))
-    model.add(Dropout(0.2))
-    model.add(Dense(output, activation='sigmoid'))
-
+    model = Sequential([
+        Dense(hidden, input_dim=inputs, name='hidden', activation='sigmoid'),
+        Dropout(0.2),
+        Dense(output, activation='sigmoid')
+    ])
     return model
 
 
 def get_saes(layers):
-    """SAEs(Stacked Auto-Encoders)
-    Build SAEs Model.
+    models = [Sequential() for _ in range(4)]
+    models[0] = _get_sae(layers[0], layers[1], layers[-1])
+    models[1] = _get_sae(layers[1], layers[2], layers[-1])
+    models[2] = _get_sae(layers[2], layers[3], layers[-1])
 
-    # Arguments
-        layers: List(int), number of input, output and hidden units.
-    # Returns
-        models: List(Model), List of SAE and SAEs.
-    """
-    sae1 = _get_sae(layers[0], layers[1], layers[-1])
-    sae2 = _get_sae(layers[1], layers[2], layers[-1])
-    sae3 = _get_sae(layers[2], layers[3], layers[-1])
-
-    saes = Sequential()
-    saes.add(Dense(layers[1], input_dim=layers[0], name='hidden1'))
-    saes.add(Activation('sigmoid'))
-    saes.add(Dense(layers[2], name='hidden2'))
-    saes.add(Activation('sigmoid'))
-    saes.add(Dense(layers[3], name='hidden3'))
-    saes.add(Activation('sigmoid'))
-    saes.add(Dropout(0.2))
-    saes.add(Dense(layers[4], activation='sigmoid'))
-
-    models = [sae1, sae2, sae3, saes]
+    models[3] = Sequential([
+        Dense(layers[1], input_dim=layers[0], name='hidden1', activation='sigmoid'),
+        Dense(layers[2], name='hidden2', activation='sigmoid'),
+        Dense(layers[3], name='hidden3', activation='sigmoid'),
+        Dropout(0.2),
+        Dense(layers[4], activation='sigmoid')
+    ])
 
     return models
